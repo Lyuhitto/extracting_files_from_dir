4c63c84062a4439412f055692ec4b6f552e6ed6b
Lyuhitto <liany0616@gmail.com>
2020-09-10 22:59:55 +0900

    Modify window size and name
    Modify window size and name.

--- ext_files_dir.py
+++ ext_files_dir.py
@@ -16,12 +16,11 @@ class MyApp(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle('Extracting Files from Directory')
         self.setWindowIcon(QIcon('img/window_icon.png'))
-        self.resize(500, 700)
         self.statusBar = self.statusBar()
         self.statusBar.showMessage('Hello')
 
+        # menubar action
         self.actionExit.triggered.connect(qApp.quit)
         self.actionView_statusbar.triggered.connect(self.toggleStat)
         self.actionView_toolbar.triggered.connect(self.toggleToolBar)
